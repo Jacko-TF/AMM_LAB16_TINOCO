6d7c3d22cb9c5eaaaef0596a6f19eaedb478f20e
Jacko <jacko.tinoco@tecsup.edu.pe>
2023-12-21 15:22:44 -0500

    Agregar archivos de proyecto.

---
+++ AMM_LAB16_TINOCO/AMM_LAB16_TINOCO/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AMM_LAB16_TINOCO
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ AMM_LAB16_TINOCO/AMM_LAB16_TINOCO/Models/ResponseBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AMM_LAB16_TINOCO.Models
+{
+    public class ResponseBase
+    {
+       public List<PlaceModel> places { get; set; }
+    }
+}
